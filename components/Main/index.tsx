f735c4a2e96cd4ae1d7a1f5ffd722e05f10d0572
osbornelive <osbornelive@live.com>
2022-01-18 11:30:09 -0300

    .
    .

---
+++ components/Main/index.tsx
@@ -0,0 +1,7 @@
+const Main = () => (
+  <div>
+    <h1>boilerplate</h1>
+  </div>
+)
+
+export default Main
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next'],
   collectCoverage: true,
-  collectCoverageFrom: ['pages/**/*.ts(x)'],
+  collectCoverageFrom: [
+    'pages/**/*.ts(x)',
+    '!pages/_app.ts(x)',
+    '!pages/_document.ts(x)'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
